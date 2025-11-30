87acafd6bcdf22f9a86ca5b2cc47d1e423420995
m-bilal-khan <bilalkhan192003@gmail.com>
2025-11-30 20:43:50 +0500

    fixes

--- backend.tf
+++ backend.tf
@@ -5,7 +5,6 @@ terraform {
   backend "s3" {
     bucket         = "cloud-fun-facts-generator-bucket"
     key            = "state/terraform.tfstate"
-    use_lockfile   = true
     encrypt        = true
     region         = "ap-south-1"
   }
